a587c5a25443ba0352d67e1523a1d558072050e1
gcs1124 <info@globalcreativeservices.us>
2026-06-04 02:28:31 +0530

    fix: adjust connected highlight end angle calculation in performance card

--- client/src/pages/ReportsPage.tsx
+++ client/src/pages/ReportsPage.tsx
@@ -208,7 +208,7 @@ function CallLeadPerformanceCard({
   const connectedHighlightStartAngle = 134;
   const connectedHighlightEndAngle =
     totalCalls > 0
-      ? connectedHighlightStartAngle -5 - (360 * safeConnectedCalls) / totalCalls
+      ? connectedHighlightStartAngle -12 - (360 * safeConnectedCalls) / totalCalls
       : connectedHighlightStartAngle;
 
   const connectedHighlightData = [
